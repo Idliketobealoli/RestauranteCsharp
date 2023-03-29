6a935b9b1e21bb7fa7bd7cc7128f73b5ee9fbdd4
Daniel Rodriguez Muñoz <daniel.rodriguez@gosupplyservices.com>
2023-03-29 10:55:53 +0200

    parte 1

---
+++ restaurante/model/Plato.cs
@@ -0,0 +1,26 @@
+﻿using App.Metrics.Concurrency;
+
+namespace restauranteCsharp.restaurante.model
+{
+    internal class Plato
+    {
+        private AtomicInteger NumeroPlatos = 0;
+        public int Id { get; set; }
+        public int Mesa { get; set; }
+        public double Precio { get; set; }
+        public Tipo TipoPlato { get; set; }
+        public Plato(int mesa, double precio, Tipo tipo) 
+        {
+            Id = NumeroPlatos;
+            NumeroPlatos++;
+
+        }
+    }
+
+    enum Tipo
+    {
+        PRIMERO,
+        SEGUNDO,
+        POSTRE
+    }
+}
