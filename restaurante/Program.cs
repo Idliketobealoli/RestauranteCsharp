5e1ee46b047cb208dfb54393f89b2834459c79aa
Daniel Rodriguez Muñoz <daniel.rodriguez@gosupplyservices.com>
2023-03-29 12:07:25 +0200

    modificacion

--- restaurante/Program.cs
+++ restaurante/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ * Restaurante: Tenemos unos cocineros que crean platos durante un tiempo indefinido. 
+ * Tenemos unos clientes que piden platos (tres por cliente, primero, segundo y postre).
+ * Los camareros sirven los platos a los clientes, siempre y cuando estos estén hechos, 
+ * y pueden servir hasta 3 platos a la vez.
+ * Cuando los clientes terminan, pagan la cuenta.
+ * Calcular el dinero total generado de 10 clientes cuando terminen. Hay 2 camareros y 3 cocineros.
+ * Lo que pagan los clientes se guarda en un fichero "pagos.txt" y al finalizar los clientes, 
+ * se calcula el total de dinero generado.
+ */
